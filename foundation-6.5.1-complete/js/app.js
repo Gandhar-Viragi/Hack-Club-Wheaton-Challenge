4f0d066ae23713dd3922594b50bd80e23ba13d37
Gandhar Viragi <48632004+Gandhar-Viragi@users.noreply.github.com>
2019-09-27 19:50:21 -0400

    Add files via upload

---
+++ foundation-6.5.1-complete/js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation()
+
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    scrollParent: true, 
+    barWidth: 3,
+    barHeight: 1,
+    height: 100,
+    hideScrollbar: true,
+    setProgressColor(white)
+    
+});
+
+wavesurfer.load('WakraSwag.mp3');
+
+wavesurfer.on('ready', function () {
+    wavesurfer.play();
+});
+
